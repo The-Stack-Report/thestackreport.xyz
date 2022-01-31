c0426ffa39ce80dbf04f73fa0f651a531466fed3
Erwin Hoogerwoord <erwinhoogerwoord@mbp-21.home>
2022-01-31 10:29:14 +0100

    added content to about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,8 +3,17 @@ import PageLayout from "components/PageLayout"
 import {
     Container
 } from "@chakra-ui/layout"
+import {
+    Box,
+    Heading
+} from "@chakra-ui/react"
+import { CMS_URL } from 'constants/cms'
+import MarkdownWrapper from "components/MarkdownWrapper"
+import _ from "lodash"
+import SocialsLinkList from "components/socials/SocialsLinkList"
 
-const AboutPage = () => {
+const AboutPage = ({ landing }) => {
+    const aboutText = _.get(landing, "attributes.about_tsr", "")
     return (
         <PageLayout>
             <Head>
@@ -13,14 +22,53 @@ const AboutPage = () => {
             </Head>
             <Container
                 maxW="container.xl"
-                paddingTop="8rem"
+                paddingTop="100px"
+                paddingBottom="8rem"
                 >
-            <h1>
-                About the stack report.
-            </h1>
+            <Box maxW="60rem" marginBottom="4rem">
+                <Heading
+                    fontWeight="thin"
+                    marginTop={{
+                        base: "2rem",
+                        md: "4rem"
+                    }}
+                    marginBottom="2rem"
+                    >
+                    About The Stack Report
+                </Heading>
+                <MarkdownWrapper
+                    markdownText={aboutText}
+                    />
+            </Box>
+            <Heading
+                fontWeight="thin"
+                marginTop={{
+                    base: "2rem",
+                    md: "4rem"
+                }}
+                marginBottom="2rem"
+                >
+                Follow on:
+            </Heading>
+            <Box maxW="20rem">
+            <SocialsLinkList />
+            </Box>
             </Container>
         </PageLayout>
     )
+}
+
+const landingPageContent = "/landing-page?populate=*"
+
+export async function getServerSideProps(context) {
+    const resp = await fetch(CMS_URL + landingPageContent)
+    const data = await resp.json()
+
+    return {
+        props: {
+            landing: data.data
+        }
+    }
 }
 
 export default AboutPage
