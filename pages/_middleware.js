352657f1d3fb2898580a4932abf33ef998586b1a
Erwin Hoogerwoord <erwinhoogerwoord@mbp-21.home>
2022-02-03 10:09:35 +0100

    removed middleware logging

--- pages/_middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export default function testMiddleware(req, res, next) {
-    const url = req.url
-    console.log("test middleware: ", url)
-    return NextResponse.rewrite(url)
-}
