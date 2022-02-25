a649c9cfa3706a7ccc32f983c01d52a3452f65b3
Erwin Hoogerwoord <erwinhoogerwoord@mbp-21.home>
2022-02-25 12:30:59 +0100

    added org logo schema data

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,14 @@ class MyDocument extends Document {
                 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
                 
                 <link rel="icon" href="/blocks-favicon.png" />
+                <script type="application/ld+json"
+                    dangerouslySetInnerHTML={{__html: JSON.stringify({
+                        "@context": "https://schema.org",
+                        "@type": "Organization",
+                        "url": "https://thestackreport.xyz",
+                        "logo": "https://thestackreport.xyz/blocks-favicon.png"
+                    })}}
+                    />
                 </Head>
                 <body>
                     <Main />
