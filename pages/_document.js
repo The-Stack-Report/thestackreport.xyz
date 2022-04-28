e221e1694676786458180fc271afb17d03f859fe
Erwin Hoogerwoord <erwinhoogerwoord@mbp-21.home>
2022-04-28 11:31:06 +0200

    tried another meta tag for font loading

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,9 @@ class MyDocument extends Document {
                 <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true.toString()}></link>
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+
                 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
 
                 <link rel="icon" href="/blocks-favicon.png" />
                 <script type="application/ld+json"
